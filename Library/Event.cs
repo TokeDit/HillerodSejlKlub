0661cac391a62240ebe45b675fc936e6ba2a7f0b
Elvie-Abarth <estibrusse18@gmail.com>
2025-04-29 12:15:07 +0200

    Add Event class

---
+++ Library/Event.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    internal class Event
+    {
+        public String Name { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public List<Member> Members { get; set; }
+        public Member CordCoordinator { get; set; }
+
+        public override string ToString()
+        {
+            return $"Navn: {Name}\nStart dato: {StartDate}\nSlut dato: {EndDate}\nMembers: {Members}\nKoordinator: {CordCoordinator}.";
+        }
+    }
+}

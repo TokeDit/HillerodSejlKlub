7d090132bd674dc89ca006b1892651f1396a5d21
Elvie-Abarth <estibrusse18@gmail.com>
2025-04-29 12:07:12 +0200

    Add Blog class.

---
+++ Library/Blog.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    internal class Blog
+    {
+        public String Name {get; set;}
+        public DateTime StartDate { get; set;}
+        public DateTime EndDate { get; set;}
+        public String Description {get; set;}
+        public Event Event {get; set;}
+        public Member Writer {get; set;}
+
+        public override string ToString()
+        {
+            return $"Navn: {Name}\nStart dato: {StartDate}\nSlut dato: {EndDate}\nBeskrivelse: {Description}\nBegivenhed: {Event}\nMedlemmer: {Writer}.";
+        }
+    }
+}

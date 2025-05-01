54f62daf3e7116c2bf83836d97fbcd758f64ff22
TokeDit <Tokedit11@gmail.com>
2025-05-01 10:09:14 +0200

    ???

--- Console/Program.cs
+++ Console/Program.cs
@@ -22,23 +22,30 @@ memberRepo.AddMember(new Member("Stefan", "Æblemadevej 1", "24613612", "Tokedit
 memberRepo.AddMember(new Member("Toke", "Æblemadevej 1", "24613612", "Tokedit11@gmail.com", Member.BoatSize.large, Member.Acceslevel.admin));
 
 //for testing, should be added runtime.
-eventRepo.AddEvent(new Event("Boat trip", new DateTime(2025, 05, 01, 12, 00, 00), new DateTime(2025, 05, 01, 18, 00, 00), memberRepo.FindMemberByName("Esti")));
+eventRepo.AddEvent(new Event("Boat trip", new DateTime(2025, 05, 01, 12, 00, 00), new DateTime(2025, 05, 01, 18, 00, 00), memberRepo.FindMemberById(1)));
 
 //for testing, should be added runtime.
-blogRepo.AddBlog(new Blog("Boat trip", new DateTime(2025, 05, 06, 12, 00, 00), new DateTime(2025, 05, 06, 18, 00, 00), "Boat trip to check boat functionality.", memberRepo.FindMemberByName("Esti")));
+blogRepo.AddBlog(new Blog("Boat trip", new DateTime(2025, 05, 06, 12, 00, 00), new DateTime(2025, 05, 06, 18, 00, 00), "Boat trip to check boat functionality.", memberRepo.FindMemberById(1)));
 
 
-bookingRepo.AddBooking(new Booking(new(2023, 12, 31, 12, 00, 00), new(2025, 1, 1, 10, 00, 00), memberRepo.FilterMemberById(1), boatRepo.FilterBoatById(1).ElementAt(0), "ingen"));
-bookingRepo.AddBooking(new Booking(new(2024, 12, 25, 10, 00, 00), new(2025, 1, 1, 10, 00, 00), memberRepo.FilterMemberById(2), boatRepo.FilterBoatById(2).ElementAt(0), "ingen"));
+bookingRepo.AddBooking(new Booking(new(2023, 12, 31, 12, 00, 00), new(2025, 1, 1, 10, 00, 00), memberRepo.FindMemberById(1), boatRepo.FindBoatById(1), "ingen"));
+bookingRepo.AddBooking(new Booking(new(2024, 12, 25, 10, 00, 00), new(2025, 1, 1, 10, 00, 00), memberRepo.FindMemberById(2), boatRepo.FindBoatById(2), "ingen"));
 
+try
+{
+    Console.WriteLine(bookingRepo.ReturnListAsString(bookingRepo.FilterBookingByBoatName("ghajfa")));
+}
+catch(NoSearhResultException e)
+{
+    Console.WriteLine(e.Message);
+}
 
-Console.WriteLine(bookingRepo.ReturnListAsString(bookingRepo.FilterBookingByBoatName("Amanda")));
 
 Console.WriteLine(memberRepo.ReturnListAsString(memberRepo.FilterMemberByName("Toke")));
 
 try
 {
-	boats.FilterBoatByName("Claus");
+    boats.FilterBoatByName("Claus");
 }
 catch (NoSearhResultException noResult)
 {

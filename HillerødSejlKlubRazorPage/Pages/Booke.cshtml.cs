ccaa5abc9a75d985218f63e3ca597925e046a91c
Elvie-Abarth <estibrusse18@gmail.com>
2025-05-02 14:30:38 +0200

    Razer page stuff

---
+++ HillerødSejlKlubRazorPage/Pages/Booke.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HillerødSejlKlubRazorPage.Pages
+{
+    public class BookeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
